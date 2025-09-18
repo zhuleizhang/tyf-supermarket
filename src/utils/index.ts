e29125a69b521e4c97e4945962a77f28b1651283
zhangzhulei <zhangzhulei.ujsw@bytedance.com>
2025-09-18 14:24:41 +0800

    日志格式问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ export const getNowTimeString = () =>
 	new Date().toLocaleString().replace(/[:/\s.]/g, '-');
 
 export const logToFile: typeof window.electron.logToFile = (...data) => {
-	return window.electron.logToFile(data);
+	return window.electron.logToFile(...data);
 };
